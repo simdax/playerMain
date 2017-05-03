1a45be099e218d3b9a6b9ef635c42e53f86d9b7c
simon <simoncornaz@gmail.com>
2017-05-03 15:18:27 +0200

    add freesound query

--- public/src/vue.js
+++ public/src/vue.js
@@ -8,6 +8,11 @@ import song from './wavesurfer';
 song.on('ready', function () {
 	vue.loading=false;
 });
+song.on("loading", function (data) {
+    console.log(data);
+    vue.percentage = data	;
+})
+
 
 document.body.onkeydown =function(e){
 	if (e.keyCode == 32) {
@@ -16,9 +21,17 @@ document.body.onkeydown =function(e){
 };
 
 
+// var form = Vue('vue', {
+// 	template = ``
+// });
+
 var vue = new Vue({
 	el:'#root',
 	data:{
+		token : "0unwI4CsQTXdXFlAu8Vj3IaCkZHEgC3H0OA4oCVl",
+		json: null,
+		instrument: "",
+		percentage: null,
 		url : "./audio/song.mp3",
 		song : song,
 		loading: true,
@@ -30,6 +43,29 @@ var vue = new Vue({
 		// api: "//www.youtubeinmp3.com/fetch/?video="
 	},
 	methods:{
+		ajax:function() {
+			var json = this.json;
+			// $.ajax(
+			// 	type: "POST",
+			// 	url:"//freesound.org/apiv2/search/text/",
+			// 	data:{query:this.instrument},
+			// 	success:function (data) {
+			// 		json = data;
+			// 	},
+			// 	dataType : "application/json"
+			// );
+			$.getJSON("//freesound.org/apiv2/search/text/?token="+this.token+"&query="+this.instrument)
+			.done(function(data){
+				if(data.count==0){
+					this.json="no data, poor boy";
+				}else{
+					this.json=data;
+				}
+			}.bind(this))
+			.fail(function(){
+				json="sum'fing went wrong boy";
+			}.bind(this))
+		},
 		fetch:function(ev){
 			this.loading = true;
 			console.log(this.url);
@@ -53,6 +89,13 @@ var vue = new Vue({
 			return 	this.loading ? this.loadMsg : 
 				this.playing ? this.playMsg : 
 				this.waitMsg;
+		},
+		jsonView: function(){
+			if(!this.json){
+				return "pas de retour";
+			}else{
+				return	this.json;
+			}
 		}
 	}
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+
+var token;
+fs.readFile('.token', (err,content)=>{
+  token = content.toString();
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/search', function(req,res){
+  var url = "https://www.freesound.org/apiv2/search/text/?token="+token+"&query="+req.body.query;
+  console.log(url);
+  res.redirect('/');
+})
 
 module.exports = router;
